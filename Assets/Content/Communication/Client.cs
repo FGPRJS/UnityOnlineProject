10a8903615e10ff4cfcbad150a71c98895b83e8a
FGCRJS <01minted.jowly@icloud.com>
2022-05-04 00:44:36 +0900

    Issue #6 - Online이 가능하게 하기 의 다음과 같은 항목 완료
    - CommunicationSpec 만들기 -> Issue Communication Spec만들기 기초 #7
    - string 메시지 보내보기
    - string 메시지 받아서 Debug.Log로 출력해보기

--- Assets/Content/Communication/Client.cs
+++ Assets/Content/Communication/Client.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text.Json;
 using UnityEngine;
+using UnityOnlineProjectProtocol;
+using UnityOnlineProjectProtocol.Connection;
+using UnityOnlineProjectProtocol.Protocol;
 
 namespace Content.Communication
 {
@@ -11,27 +15,33 @@ namespace Content.Communication
         private string _ip;
         private int _port;
 
+        private byte[] sendBuffer;
+
         public Client(string ip, int port)
         {
             _ip = ip;
             _port = port;
-            
+
             socket = new Socket(
-                AddressFamily.InterNetwork, 
-                SocketType.Stream, 
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
                 ProtocolType.Tcp);
+
+            socket.ReceiveBufferSize = AsyncStateObject.BufferSize;
+            socket.SendBufferSize = AsyncStateObject.BufferSize;
         }
 
         public void Connect()
         {
+            Debug.Log("Trying to connect to server...");
+            
             IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
             IPAddress ipAddress = ipHostInfo.AddressList[1];
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, _port);
 
             try
             {
-                Debug.Log("Trying to connect with Server...");
-                socket.BeginConnect(localEndPoint, ServerConnected, socket);
+                socket.BeginConnect(localEndPoint, Connected, socket);
             }
             catch (Exception ex)
             {
@@ -39,9 +49,46 @@ namespace Content.Communication
             }
         }
 
-        private void ServerConnected(IAsyncResult ar)
+        private void Connected(IAsyncResult ar)
         {
-            Debug.Log("Connect with server Complete!");
+            socket.EndConnect(ar);
+            
+            Debug.Log("Successfully connected!");
+            
+            //Temp
+            CommunicationMessage congratulationMessage = new CommunicationMessage();
+            congratulationMessage.Message = "Hello! I'm new client!";
+            SendData(congratulationMessage);
+        }
+
+        public void SendData(CommunicationMessage message)
+        {
+            Debug.Log("Trying to send...");
+
+            try
+            {
+                var data = CommunicationUtility.Serialize(message);
+
+                socket.BeginSend(
+                    data,
+                    0,
+                    data.Length,
+                    SocketFlags.None,
+                    SendComplete,
+                    socket);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Send Failure. Reason : " + ex.Message);
+            }
+            
+        }
+
+        private void SendComplete(IAsyncResult ar)
+        {
+            socket.EndSend(ar);
+            
+            Debug.Log("Send Complete!");
         }
     }
 }
